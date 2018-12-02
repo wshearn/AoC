56f4fa3204ac434a9ffafdf61cfe44876319b9c0
Wesley Hearn <wesley@shearn.me>
2018-12-01 20:44:48 -0500

    Update Main.hx
    > Executing task: C:\HaxeToolkit\haxe\haxe.exe --connect 6000 build.hxml <
    
    Main.hx:41: Part 1 Result: 590
    Main.hx:42: Part 2 Result: 83780
    Main.hx:43: Part 2 took 142 loops
    Main.hx:44: Time in seconds it took to run: 0.82579350471496582
    
    Terminal will be reused by tasks, press any key to close it.

--- Day1/src/Main.hx
+++ Day1/src/Main.hx
@@ -1,45 +1,46 @@
+import haxe.Timer;
 import haxe.io.Eof;
 import sys.io.File;
 
 class Main {
 	static function main() {
+		var loops = 0;
 		var result:Int = 0;
 		var initialResult:Int = 0;
 		var initialResultFound:Bool = false;
 		var dupeResult:Int = 0;
 		var dupeFound:Bool = false;
-
 		var dupe = new Map<Int, Bool>();
+		
+		var stamp = Timer.stamp();
+		var data = File.getContent("input").split('\n');
 		while (!dupeFound) {
-			var file = File.read("input");
-			try {
-				while (true) {
-					var line = file.readLine();
-					var dir = line.charAt(0);
-					var num = Std.parseInt(line.substr(1, line.length));
-
-					if (!dupeFound) dupe[result] = true;
-					if (dir == '+') {
-						result += num;
-					} else {
-						result -= num;
-					}
-					if (dupe.exists(result)) {
-						dupeResult = result;
-						dupeFound = true;
-					}
+			loops++;
+			for (x in data) {
+				var dir = x.charAt(0);
+				var num = Std.parseInt(x.substr(1, x.length));
+				if (num == null) break;
+				if (!dupeFound) dupe[result] = true;
+				if (dir == '+') {
+					result += num;
+				} else {
+					result -= num;
 				}
-			}
-			catch ( ex:Eof) {
-				file.close();
+				if (dupe.exists(result)) {
+					dupeResult = result;
+					dupeFound = true;
+				}
 			}
 			if (!initialResultFound) {
 				initialResult = result;
 				initialResultFound = true;
 			}
-
 		}
+		var stopStamp = Timer.stamp();
+
 		trace("Part 1 Result: " + initialResult);
 		trace("Part 2 Result: " + dupeResult);
+		trace("Part 2 took " + loops + " loops");
+		trace("Time in seconds it took to run: " + (stopStamp-stamp));
 	}
 }
