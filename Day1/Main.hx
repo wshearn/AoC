6707559c1af9200bf58bd6c4849f2418d9e562a9
Wesley Hearn <wesley@shearn.me>
2018-12-03 09:47:03 -0500

    Fix tabs

--- Day1/Main.hx
+++ Day1/Main.hx
@@ -2,51 +2,52 @@ import haxe.Timer;
 import sys.io.File;
 
 class Main {
-	static function main() {
-		var loops = 0;
-		var result:Int = 0;
-		var initialResult:Int = 0;
-		var initialResultFound:Bool = false;
-		var dupeResult:Int = 0;
-		var dupeFound:Bool = false;
-		var dupe = new Map<Int, Bool>();
+    static function main() {
+        var loops = 0;
+        var result:Int = 0;
+        var initialResult:Int = 0;
+        var initialResultFound:Bool = false;
+        var dupeResult:Int = 0;
+        var dupeFound:Bool = false;
+        var dupe = new Map<Int, Bool>();
 		
-		var stamp = Timer.stamp();
-		var data = File.getContent("input").split('\n');
-		data.remove("");
-		while (true) {
-			loops++;
-			for (change in data) {
-				var dir = change.charAt(0);
-				var num = Std.parseInt(change.substr(1, change.length));
+        var data = File.getContent("input").split('\n');
+        data.remove("");
+
+        var stamp = Timer.stamp();
+        while (true) {
+            loops++;
+            for (change in data) {
+                var dir = change.charAt(0);
+                var num = Std.parseInt(change.substr(1, change.length));
                 
-				if (!dupeFound) dupe[result] = true;
+                if (!dupeFound) dupe[result] = true;
                 
-				if (dir == '+') {
-					result += num;
-				} else {
-					result -= num;
-				}
+                if (dir == '+') {
+                    result += num;
+                } else {
+                    result -= num;
+                }
                 
-				if (dupeFound == false && dupe.exists(result)) {
-					dupeResult = result;
-					dupeFound = true;
-				}
-			}
-			if (!initialResultFound) {
-				initialResult = result;
-				initialResultFound = true;
+                if (dupeFound == false && dupe.exists(result)) {
+                    dupeResult = result;
+                    dupeFound = true;
+                }
+            }
+            if (!initialResultFound) {
+                initialResult = result;
+                initialResultFound = true;
 			}
             
-			if (initialResultFound && dupeFound) {
-				break;
-			}
-		}
-		var stopStamp = Timer.stamp();
+            if (initialResultFound && dupeFound) {
+                break;
+            }
+        }
+        var stopStamp = Timer.stamp();
         
-		trace("Part 1 Result: " + initialResult);
-		trace("Part 2 Result: " + dupeResult);
-		trace("Part 2 took " + loops + " loops");
-		trace("Time in seconds it took to run: " + (stopStamp-stamp));
-	}
+        trace("Part 1 Result: " + initialResult);
+        trace("Part 2 Result: " + dupeResult);
+        trace("Part 2 took " + loops + " loops");
+        trace("Time in seconds it took to run: " + (stopStamp-stamp));
+    }
 }
--- Day2/Main.hx
+++ Day2/Main.hx
@@ -3,66 +3,66 @@ import sys.io.File;
 
 class Main {
 	static function main() {
-		var two = 0;
-		var three = 0;
+        var two = 0;
+        var three = 0;
         
-		var currentMatch = {
-			"key1": 0,
+        var currentMatch = {
+            "key1": 0,
             "key2": 0,
-			"diff": 9999
-		}
+            "diff": 9999
+        }
         
-		var data = File.getContent("input").split('\n');
-		data.remove("");
-        
-		var stamp = Timer.stamp();
-		for (x in 0...data.length) {
-			// Part 1
+        var data = File.getContent("input").split('\n');
+        data.remove("");
+
+        var stamp = Timer.stamp();
+        for (x in 0...data.length) {
+            // Part 1
 			var nextCode:Int = x+1;
-			var labelHash = new Map<String, Int>();
-			var idSplit = data[x].split("");
-			for (char in idSplit) {
-				if (!labelHash.exists(char)) {
-					labelHash[char] = 1;
-				} else {
-					labelHash[char] += 1;
-				}
-			}
+            var labelHash = new Map<String, Int>();
+            var idSplit = data[x].split("");
+            for (char in idSplit) {
+                if (!labelHash.exists(char)) {
+                    labelHash[char] = 1;
+                } else {
+                    labelHash[char] += 1;
+                }
+            }
             
-			var twoUsed:Bool = false;
-			var threeUsed:Bool = false;
-			for (char in labelHash.keys()) {
-				if (!twoUsed && labelHash[char] == 2) {
-					two++;
-					twoUsed = true;
-				} else if (!threeUsed && labelHash[char] == 3) {
-					three++;
-					threeUsed = true;
-				}
+            var twoUsed:Bool = false;
+            var threeUsed:Bool = false;
+            for (char in labelHash.keys()) {
+                if (!twoUsed && labelHash[char] == 2) {
+                    two++;
+                    twoUsed = true;
+                } else if (!threeUsed && labelHash[char] == 3) {
+                    three++;
+                    threeUsed = true;
+                }
                 
-				if (threeUsed && twoUsed) {
-					break;
-				}
-			}
-			// End Part 1
+                if (threeUsed && twoUsed) {
+                    break;
+                }
+            }
+            // End Part 1
             
-			// Part 2
-			for (y in nextCode...data.length) {
-				var diff:Int = 0;
-				var same:String = "";
-				for (z in 0...data[x].length) {
-					if (data[x].charAt(z) != data[y].charAt(z)) {
-						diff += 1;
-					}
-				}
-				if (currentMatch.diff > diff) {
-					currentMatch.diff = diff;
-					currentMatch.key1 = x;
+            // Part 2
+            for (y in nextCode...data.length) {
+                var diff:Int = 0;
+                var same:String = "";
+                for (z in 0...data[x].length) {
+                    if (data[x].charAt(z) != data[y].charAt(z)) {
+                        diff += 1;
+                    }
+                }
+                if (currentMatch.diff > diff) {
+                    currentMatch.diff = diff;
+                    currentMatch.key1 = x;
                     currentMatch.key2 = y;
-				}
-			}
-			// End Part 2
-		}
+                }
+            }
+            // End Part 2
+        }
         // Part 2 - Get Common chars
         var sameKey = "";
         for (x in 0...data[currentMatch.key1].length) {
@@ -72,10 +72,10 @@ class Main {
         }
         // End Part 2
         
-		var stopStamp = Timer.stamp();
+        var stopStamp = Timer.stamp();
         
-		trace("Checksum: " + (two * three));
-		trace("Same key: " + sameKey);
-		trace("Time in seconds it took to run part1: " + (stopStamp-stamp));
-	}
+        trace("Checksum: " + (two * three));
+        trace("Same key: " + sameKey);
+        trace("Time in seconds it took to run part1: " + (stopStamp-stamp));
+    }
 }
