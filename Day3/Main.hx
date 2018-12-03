fff055339dbca45b1debaf4cf2d1989c13dff7ad
Wesley Hearn <wesley@shearn.me>
2018-12-03 02:33:04 -0500

    Update Main.hx
    Only loop through the entire data set once.

--- Day3/Main.hx
+++ Day3/Main.hx
@@ -9,16 +9,19 @@ class Main {
         data.remove("");
         var fabric:Array<Array<Int>> = [ for (x in 0...1000) [ for (y in 0...1000) 0 ]];
         var used = 0;
+        var goodSquares = new List<Int>();
         var singleSquare = "";
         for (square in data) {
-            var cords = square.split(' ')[2].split(',');
+            var goodSquare:Bool = true;
+            var squareSplit = square.split(' ');
+            var cords = squareSplit[2].split(',');
             cords[1] = cords[1].substring(0, cords[1].length-1);
             
             var x = Std.parseInt(cords[0]);
             var y = Std.parseInt(cords[1]);
             
             
-            var size = square.split(' ')[3].split('x');
+            var size = squareSplit[3].split('x');
             var x2 = x+Std.parseInt(size[0]);
             var y2 = y+Std.parseInt(size[1]);
             
@@ -28,23 +31,28 @@ class Main {
                     if (fabric[x3][y3] == 2) {
                         used++;
                     }
+                    if (fabric[x3][y3] > 1) {
+                        goodSquare = false;
+                    }
                 }
+            }
+            if (goodSquare) {
+                goodSquares.add(Std.parseInt(squareSplit[0].substring(1, squareSplit[0].length)));
             }
         }
         
-        for (square in data) {
-            var goodSquare:Bool = true;
-            var cords = square.split(' ')[2].split(',');
+        for (gSquare in goodSquares) {
+            var square = data[gSquare-1];
+            var squareSplit = square.split(' ');
+            var cords = squareSplit[2].split(',');
             cords[1] = cords[1].substring(0, cords[1].length-1);
             
             var x = Std.parseInt(cords[0]);
             var y = Std.parseInt(cords[1]);
-            
-            
-            var size = square.split(' ')[3].split('x');
+            var size = squareSplit[3].split('x');
             var x2 = x+Std.parseInt(size[0]);
             var y2 = y+Std.parseInt(size[1]);
-            
+            var goodSquare:Bool = true;
             for (x3 in x...x2) {
                 for (y3 in y...y2) {
                     if (fabric[x3][y3] > 1) {
@@ -52,12 +60,13 @@ class Main {
                     }
                 }
             }
-            
             if (goodSquare) {
-                singleSquare = square.split(' ')[0];
+                singleSquare = squareSplit[0].substring(1, squareSplit[0].length);
                 break;
             }
+            
         }
+        
         var stopStamp = Timer.stamp();
         
         trace("Part 1 Result: " + used);
