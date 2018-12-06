8e203e3c62f6abf843aa32e8b8b37db4bfd419fa
Wesley Hearn <wesley@shearn.me>
2018-12-06 12:38:15 -0500

    Day 6 done, takes 2 mins to run

--- Day6/Main.hx
+++ Day6/Main.hx
@@ -16,8 +16,8 @@ class Main {
         }
         // Find the bottom right most cord
         for (cord in data) {
-            var x = Std.parseInt(cord.split(", ")[0]);
-            var y = Std.parseInt(cord.split(", ")[1]);
+            var x = Std.parseInt(cord.split(", ")[1]);
+            var y = Std.parseInt(cord.split(", ")[0]);
 
             if (bottomRight.x < x) {
                 bottomRight.x = x;
@@ -26,34 +26,52 @@ class Main {
                 bottomRight.y = y;
             }
         }
-        bottomRight.x += 1;
-        bottomRight.y += 1;
+        bottomRight.x += 100;
+        bottomRight.y += 100;
 
         var count:Array<Int> = [ for (x in 0...data.length) 0 ];
+        var glob:Int = 0;
+        var globDist:Int = 10000;
 
         for (x in 0...bottomRight.x) {
+
             for (y in 0...bottomRight.y) {
                 var closestDist:Int = 999999;
                 var closestWhich:Int = -1;
+                var pointDistGlob:Int = 0;
 
                 for (z in 0...data.length) {
-                    var cordx = Std.parseInt(data[z].split(", ")[0]);
-                    var cordy = Std.parseInt(data[z].split(", ")[1]);
+                    var cordx = Std.parseInt(data[z].split(", ")[1]);
+                    var cordy = Std.parseInt(data[z].split(", ")[0]);
 
                     var xd = Std.int(Math.abs(x - cordx));
                     var yd = Std.int(Math.abs(y - cordy));
 
                     var dist = xd+yd;
+                    if (dist == closestDist) {
+                        closestWhich = -1;
+                    }
+
+                    if (x == cordx && y == cordy) {
+                        closestDist = -1;
+                        closestWhich = z;
+                    }
+ 
                     if (dist < closestDist) {
                         closestDist = dist;
                         closestWhich = z;
                     }
+                    pointDistGlob += dist;
                 }
                 if (x == 0 || x == bottomRight.x-1 || y == 0 || y == bottomRight.y-1) {
                     count[closestWhich] = -1;
                 }
                 if (closestWhich != -1 && count[closestWhich] != -1) {
                     count[closestWhich] += 1;
+                }
+
+                if (pointDistGlob < globDist) {
+                    glob++;
                 }
             }
         }
@@ -68,7 +86,7 @@ class Main {
         var stopStamp = Timer.stamp();
 
         trace("Part 1 Result: " + count[0]);
-        trace("Part 2 Result: ");
+        trace("Part 2 Result: " + glob);
         trace("Time in seconds it took to run: " + (stopStamp-stamp));
     }
 }
