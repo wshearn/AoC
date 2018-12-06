b7f4be346e0ebd6642e3b45b438e265363c02e38
Wesley Hearn <wesley@shearn.me>
2018-12-06 02:01:54 -0500

    Update Main.hx
    i'm running out of ideas :/

--- Day6/Main.hx
+++ Day6/Main.hx
@@ -16,8 +16,8 @@ class Main {
         }
         // Find the bottom right most cord
         for (cord in data) {
-            var x = Std.parseInt(cord.split(", ")[0]) + 1;
-            var y = Std.parseInt(cord.split(", ")[1]) + 1;
+            var x = Std.parseInt(cord.split(", ")[0]);
+            var y = Std.parseInt(cord.split(", ")[1]);
 
             if (bottomRight.x < x) {
                 bottomRight.x = x;
@@ -26,47 +26,48 @@ class Main {
                 bottomRight.y = y;
             }
         }
-        var grid:Array<Array<Int>> = [ for (x in 0...bottomRight.x) [ for (y in 0...bottomRight.y) 0 ]];
+        bottomRight.x += 1;
+        bottomRight.y += 1;
+        var grid:Array<Array<Int>> = [ for (x in 0...bottomRight.x) [ for (y in 0...bottomRight.y) -1 ]];
         var count:Array<Int> = [ for (x in 0...data.length+1) 0 ];
         for (z in 0...data.length) {
             var x = Std.parseInt(data[z].split(", ")[0]);
             var y = Std.parseInt(data[z].split(", ")[1]);
-            grid[x][y] = z+1;
+            grid[x][y] = z;
         }
 
         for (x in 0...bottomRight.x) {
             for (y in 0...bottomRight.y) {
                 var closestDist:Float = 999999.0;
-                var closestWhich:Int = 0;
+                var closestWhich:Int = -1;
 
                 // Something is fishy here
                 for (z in 0...data.length) {
-                    var cordx = Std.parseInt(data[z].split(", ")[0]) + 1;
-                    var cordy = Std.parseInt(data[z].split(", ")[1]) + 1;
+                    var cordx = Std.parseInt(data[z].split(", ")[0]);
+                    var cordy = Std.parseInt(data[z].split(", ")[1]);
 
-                    var xd = x-cordx;
-                    var yd = y-cordy;
+                    var xd = cordx-x;
+                    var yd = cordy-y;
 
-                    var dist = Math.sqrt(xd*xd + yd*yd);
+                    var dist = Math.round(Math.sqrt(xd*xd + yd*yd));
                     if (dist < closestDist) {
                         closestDist = dist;
                         closestWhich = z+1;
                     } else if (dist == closestDist) {
                         closestDist = 0;
-                        closestWhich = 0;
+                        closestWhich = -1;
                     }
                 }
                 grid[x][y] = closestWhich;
                 if (x == 0 || x == bottomRight.x || y == 0 || y == bottomRight.y) {
                     count[closestWhich] = -1;
                 }
-                if (closestWhich != 0 && count[closestWhich] != -1) {
+                if (closestWhich != -1 && count[closestWhich] != -1) {
                     count[closestWhich] += 1;
                 }
             }
         }
 
-        trace(grid);
         trace(count);
         
         var stopStamp = Timer.stamp();
