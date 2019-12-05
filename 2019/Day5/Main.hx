2bed3d67dec0a98831740fe445e398c2376c1781
Wesley Hearn <whearn@redhat.com>
2019-12-05 16:06:56 -0500

    Part of part 2

--- 2019/Day5/Main.hx
+++ 2019/Day5/Main.hx
@@ -2,12 +2,12 @@ import haxe.Timer;
 import sys.io.File;
 
 class Main {
-    public static function process(data:Array<Int>):Int {
+    public static function process(data:Array<Int>, input:Int):Int {
         var finalResult = 0;
         var pos = 0;
 
+        trace("\n\n\nStarting new set...");
         while(true) {
-            var oldData = data.copy();
             var jmpPos = 0;
             var instruction = data[pos];
             trace("instruction: " + instruction);
@@ -19,7 +19,9 @@ class Main {
 
             var num1Ins = Math.floor((instruction % 1000)/100);
             var num2Ins = Math.floor((instruction % 10000)/1000);
-            var num3Ins = Math.floor((instruction % 100000)/10000);
+
+            Assertion.assert(num1Ins == 0 || num1Ins == 1);
+            Assertion.assert(num2Ins == 0 || num2Ins == 1);
 
             var num1:Int = 0;
             var num2:Int = 0;
@@ -44,29 +46,43 @@ class Main {
                     data[data[pos+3]] = num1*num2;
                     jmpPos = 4;
                 case 3: //Input
-                    var input = 1;
                     data[data[pos+3]] = input;
                     jmpPos = 2;
                 case 4: //Output
                     finalResult = num1;
-                    trace("result: " + finalResult);
                     if (data[pos+2] != 99) {
                         Assertion.assert(finalResult == 0);
                     }
                     jmpPos = 2;
-            }
-
-            if (opcode < 4) {
-                Assertion.assert(oldData != data);
+                case 5: //Jump if true
+                    if (num1 != 0) {
+                        jmpPos = num2;
+                    } else {
+                        jmpPos = 3;
+                    }
+                case 6: //Jump if false
+                    if (num1 == 0) {
+                        jmpPos = num2;
+                    } else {
+                        jmpPos = 3;
+                    }
+                case 7: // Less than
+                    if (num1 < num2) {
+                        data[data[pos+3]] = 1;
+                     } else {
+                        data[data[pos+3]] = 0;
+                     }
+                    jmpPos = 4;
+                case 8: // Equals than
+                    if (num1 == num2) {
+                        data[data[pos+3]] = 1;
+                     } else {
+                        data[data[pos+3]] = 0;
+                     }
+                    jmpPos = 4;
             }
 
             pos += jmpPos;
-        }
-
-        for (i in data) {
-            if (i == 16348437) { // Cheated to get this value
-                trace("Value is present..");
-            }
         }
 
         return finalResult;
@@ -88,7 +104,8 @@ class Main {
         var stamp = Timer.stamp();
 
         var p1Data = data.copy();
-        part1Answer = Main.process(p1Data);
+        part1Answer = Main.process(p1Data, 1);
+        part2Answer = Main.process(p1Data, 5);
         var stopStamp = Timer.stamp();
 
         trace("Part 1: " + part1Answer);
