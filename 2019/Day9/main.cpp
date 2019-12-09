eb4ca4d902e8de6075ec78675d44d4969145e1a1
Wesley Hearn <whearn@redhat.com>
2019-12-09 16:14:03 -0500

    Astyle

--- 2019/Day9/main.cpp
+++ 2019/Day9/main.cpp
@@ -8,145 +8,154 @@ using namespace std;
 
 class IntComputer
 {
-private:
-    /* data */
-public:
-    long long process(long long data[], long long input)
-    {
-        long long finalResult = 0;
-        int pos = 0;
-        int relativeBase = 0;
+    private:
+        /* data */
+    public:
+        long long process(long long data[], long long input) {
+            long long result = 0;
+            int pos = 0;
+            int relativeBase = 0;
 
-        while (true)
-        {
-            long long instruction = data[pos];
-            int opcode = (instruction % 100);
+            while (true) {
+                auto instruction = data[pos];
+                int opcode = (instruction % 100);
 
-            if (opcode == 99) // Exit
-            {
-                break;
-            }
+                if (opcode == 99) { // Exit
+                    break;
+                }
 
-            int num1Ins = (instruction % 1000) / 100;
-            int num2Ins = (instruction % 10000) / 1000;
-            int num3Ins = (instruction % 100000) / 10000;
-            long long num1 = 0;
-            long long num2 = 0;
-            long long num3 = 0;
+                auto num1Ins = (instruction % 1000) / 100;
+                auto num2Ins = (instruction % 10000) / 1000;
+                auto num3Ins = (instruction % 100000) / 10000;
+                auto num1 = 0L;
+                auto num2 = 0L;
+                auto num3 = 0L;
 
-            switch (num1Ins)
-            {
-            case 1:
-                num1 = pos + 1;
-                break;
-            case 2:
-                num1 = data[pos + 1] + relativeBase;
-                break;
-            default:
-                num1 = data[pos + 1];
-                break;
-            }
-            switch (num2Ins)
-            {
-            case 1:
-                num2 = pos + 2;
-                break;
-            case 2:
-                num2 = data[pos + 2] + relativeBase;
-                break;
-            default:
-                num2 = data[pos + 2];
-                break;
-            }
+                switch (num1Ins) {
+                    case 1:
+                        num1 = pos + 1;
+                        break;
 
-            switch (num3Ins)
-            {
-            case 1:
-                num3 = pos + 3;
-                break;
-            case 2:
-                num3 = data[pos + 3] + relativeBase;
-                break;
-            default:
-                num3 = data[pos + 3];
-                break;
-            }
+                    case 2:
+                        num1 = data[pos + 1] + relativeBase;
+                        break;
 
-            switch (opcode)
-            {
-            case 1: // Addition
-                data[num3] = data[num1] + data[num2];
-                pos += 4;
-                break;
-            case 2: // Multiplication
-                data[num3] = data[num1] * data[num2];
-                pos += 4;
-                break;
-            case 3: // Input
-                data[num1] = input;
-                pos += 2;
-                break;
-            case 4: // Output
-                finalResult = data[num1];
-                pos += 2;
-                break;
-            case 5: // Jump if True
-                data[num1] != 0 ? pos = data[num2] : pos += 3;
-                break;
-            case 6: // Jump if False
-                data[num1] == 0 ? pos = data[num2] : pos += 3;
-                break;
-            case 7: // Less than
-                data[num1] < data[num2] ? data[num3] = 1 : data[num3] = 0;
-                pos += 4;
-                break;
-            case 8: // Equals to
-                data[num1] == data[num2] ? data[num3] = 1 : data[num3] = 0;
-                pos += 4;
-                break;
-            case 9: // Relative base
-                relativeBase += data[num1];
-                pos += 2;
-                break;
-            default:
-                break;
-            }
+                    default:
+                        num1 = data[pos + 1];
+                        break;
+                }
+
+                switch (num2Ins) {
+                    case 1:
+                        num2 = pos + 2;
+                        break;
+
+                    case 2:
+                        num2 = data[pos + 2] + relativeBase;
+                        break;
+
+                    default:
+                        num2 = data[pos + 2];
+                        break;
+                }
+
+                switch (num3Ins) {
+                    case 1:
+                        num3 = pos + 3;
+                        break;
+
+                    case 2:
+                        num3 = data[pos + 3] + relativeBase;
+                        break;
+
+                    default:
+                        num3 = data[pos + 3];
+                        break;
+                }
+
+                switch (opcode) {
+                    case 1: // Addition
+                        data[num3] = data[num1] + data[num2];
+                        pos += 4;
+                        break;
+
+                    case 2: // Multiplication
+                        data[num3] = data[num1] * data[num2];
+                        pos += 4;
+                        break;
+
+                    case 3: // Input
+                        data[num1] = input;
+                        pos += 2;
+                        break;
+
+                    case 4: // Output
+                        result = data[num1];
+                        pos += 2;
+                        break;
+
+                    case 5: // Jump if True
+                        data[num1] != 0 ? pos = data[num2] : pos += 3;
+                        break;
+
+                    case 6: // Jump if False
+                        data[num1] == 0 ? pos = data[num2] : pos += 3;
+                        break;
+
+                    case 7: // Less than
+                        data[num1] < data[num2] ? data[num3] = 1 : data[num3] = 0;
+                        pos += 4;
+                        break;
+
+                    case 8: // Equals to
+                        data[num1] == data[num2] ? data[num3] = 1 : data[num3] = 0;
+                        pos += 4;
+                        break;
+
+                    case 9: // Relative base
+                        relativeBase += data[num1];
+                        pos += 2;
+                        break;
+
+                    default:
+                        break;
+                }
+            };
+
+            return result;
         };
-
-        return finalResult;
-    };
-    IntComputer(){};
-    ~IntComputer(){};
+        IntComputer() {};
+        ~IntComputer() {};
 };
 
 int main()
 {
-    IntComputer *intComputer = new IntComputer();
+    auto intComputer = new IntComputer();
     ifstream input("input");
     string raw_data((istreambuf_iterator<char>(input)),
                     istreambuf_iterator<char>());
 
     auto start = chrono::system_clock::now();
 
-    size_t n = count(raw_data.begin(), raw_data.end(), ',') + 1;
-
-    long long *data;
-    data = new long long[n];
+    auto n = count(raw_data.begin(), raw_data.end(), ',') + 1;
+    auto data = new long long[n];
 
     size_t pos = 0;
     string token;
     int count = 0;
-    while ((pos = raw_data.find(',')) != string::npos)
-    {
+
+    while ((pos = raw_data.find(',')) != string::npos) {
         token = raw_data.substr(0, pos);
         raw_data.erase(0, pos + 1);
         data[count] = stoll(token);
         count++;
     }
-    data[n - 1] = stoll(raw_data);
 
-    long long part1Answer = intComputer->process(data, 1);
-    long long part2Answer = intComputer->process(data, 2);
+    data[n - 1] = stoll(raw_data);
+    auto part1Answer = intComputer->process(data, 1);
+    assert(part1Answer == 3063082071);
+    auto part2Answer = intComputer->process(data, 2);
+    assert(part2Answer == 81348);
 
     auto end = chrono::system_clock::now();
     chrono::duration<double> elapsed_seconds = end-start;
