33dc8f96bd6df375d6037fd034b17c6488391a42
Wesley Hearn <whearn@redhat.com>
2019-12-09 15:57:07 -0500

    Finished Day 9

--- 2019/Day9/main.cpp
+++ 2019/Day9/main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
 #include <fstream>
-#include <string>
+#include <iostream>
 #include <streambuf>
+#include <string>
+#include <chrono>
 
 using namespace std;
 
@@ -14,11 +15,13 @@ public:
     {
         long long finalResult = 0;
         int pos = 0;
+        int relativeBase = 0;
 
         while (true)
         {
             long long instruction = data[pos];
             int opcode = (instruction % 100);
+
             if (opcode == 99) // Exit
             {
                 break;
@@ -26,100 +29,84 @@ public:
 
             int num1Ins = (instruction % 1000) / 100;
             int num2Ins = (instruction % 10000) / 1000;
-
-            assert(num1Ins >= 0 && num1Ins <= 2);
-            assert(num2Ins >= 0 && num2Ins <= 2);
-
+            int num3Ins = (instruction % 100000) / 10000;
             long long num1 = 0;
             long long num2 = 0;
+            long long num3 = 0;
 
             switch (num1Ins)
             {
-            case 0:
-                num1 = data[data[pos + 1]];
-                break;
             case 1:
-                num1 = data[pos + 1];
+                num1 = pos + 1;
                 break;
             case 2:
+                num1 = data[pos + 1] + relativeBase;
                 break;
             default:
+                num1 = data[pos + 1];
                 break;
             }
             switch (num2Ins)
             {
-            case 0:
-                num2 = data[data[pos + 2]];
-                break;
             case 1:
-                num2 = data[pos + 1];
+                num2 = pos + 2;
                 break;
             case 2:
+                num2 = data[pos + 2] + relativeBase;
                 break;
             default:
+                num2 = data[pos + 2];
+                break;
+            }
+
+            switch (num3Ins)
+            {
+            case 1:
+                num3 = pos + 3;
+                break;
+            case 2:
+                num3 = data[pos + 3] + relativeBase;
+                break;
+            default:
+                num3 = data[pos + 3];
                 break;
             }
 
             switch (opcode)
             {
             case 1: // Addition
-                data[data[pos + 3]] = num1 + num2;
+                data[num3] = data[num1] + data[num2];
                 pos += 4;
                 break;
             case 2: // Multiplication
-                data[data[pos + 3]] = num1 * num2;
+                data[num3] = data[num1] * data[num2];
                 pos += 4;
                 break;
             case 3: // Input
-                data[data[pos + 1]] = input;
+                data[num1] = input;
                 pos += 2;
                 break;
             case 4: // Output
-                finalResult = num1;
-                cout << num1 << endl;
+                finalResult = data[num1];
                 pos += 2;
                 break;
             case 5: // Jump if True
-                if (num1 != 0)
-                {
-                    pos = num2;
-                }
-                else
-                {
-                    pos += 3;
-                }
+                data[num1] != 0 ? pos = data[num2] : pos += 3;
                 break;
             case 6: // Jump if False
-                if (num1 == 0)
-                {
-                    pos = num2;
-                }
-                else
-                {
-                    pos += 3;
-                }
+                data[num1] == 0 ? pos = data[num2] : pos += 3;
                 break;
             case 7: // Less than
-                if (num1 < num2)
-                {
-                    data[data[pos + 3]] = 1;
-                }
-                else
-                {
-                    data[data[pos + 3]] = 0;
-                }
+                data[num1] < data[num2] ? data[num3] = 1 : data[num3] = 0;
                 pos += 4;
                 break;
             case 8: // Equals to
-                if (num1 == num2)
-                {
-                    data[data[pos + 3]] = 1;
-                }
-                else
-                {
-                    data[data[pos + 3]] = 0;
-                }
+                data[num1] == data[num2] ? data[num3] = 1 : data[num3] = 0;
                 pos += 4;
+                break;
+            case 9: // Relative base
+                relativeBase += data[num1];
+                pos += 2;
                 break;
             default:
                 break;
@@ -139,6 +126,8 @@ int main()
     string raw_data((istreambuf_iterator<char>(input)),
                     istreambuf_iterator<char>());
 
+    auto start = std::chrono::system_clock::now();
+
     size_t n = count(raw_data.begin(), raw_data.end(), ',') + 1;
 
     long long *data;
@@ -156,7 +145,16 @@ int main()
     }
     data[n - 1] = stoll(raw_data);
 
-    intComputer->process(data, 1);
+    long long part1Answer = intComputer->process(data, 1);
+    long long part2Answer = intComputer->process(data, 2);
+
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    cout << "Part 1 Answer: " << part1Answer << endl;
+    cout << "Part 2 Answer: " << part2Answer << endl;
+    cout << "Time it took to run: " << elapsed_seconds.count() << endl;
 
     return 0;
 }
---
+++ 2019/Day9/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+g++ -std=c++11 main.cpp
+./a.out
+rm -f a.out
