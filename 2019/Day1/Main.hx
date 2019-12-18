1cbeb3556d1e9ce8aaa3fe4e2af4ca30a2a127b2
Mighty Derp <themightyderp@dirtyninjagames.io>
2019-12-18 10:06:48 -0500

    Update Main.hx
    changed day 1

--- 2019/Day1/Main.hx
+++ 2019/Day1/Main.hx
@@ -2,53 +2,43 @@ import haxe.Timer;
 import sys.io.File;
 
 class Main {
-    public static function main() {
-        var part1Answer = 0;
-        var part2Answer = 0;
+	public static function part2Recursion(needed:Int, moduleFuel:Int):Int {
+		needed = Math.floor(needed / 3) - 2;
+		if (needed < 0) {
+			return moduleFuel;
+		}
+		moduleFuel += needed;
 
-        var totalFuel = 0;
+		return part2Recursion(needed, moduleFuel);
+	}
 
-        var fileIn = File.read("input", false);
-        var stamp = Timer.stamp();
-        try {
-            while(true) {
-                var line = fileIn.readLine();
-                var needed = Std.parseInt(line);
-                totalFuel += Math.floor(needed/3)-2;
-            }
-        } catch (e:haxe.io.Eof) {
-            fileIn.close();
-        }
+	public static function main() {
+		var part1Answer = 0;
+		var part2Answer = 0;
 
-        part1Answer = totalFuel;
+		var fileIn = File.read("input", false);
+		var stamp = Timer.stamp();
 
-        // Part 2
-        var totalFuelOfFuel = 0;
-        fileIn = sys.io.File.read("input", false);
-        try {
-            while(true) {
-                var line = fileIn.readLine();
-                var needed = Std.parseInt(line);
-                var moduleFuel = 0;
-                var currentFuel = 0;
-                while(currentFuel >= 0) {
-                    currentFuel = Math.floor(needed/3)-2;
-                    if (currentFuel > 0) {
-                        moduleFuel += currentFuel;
-                    }
-                    needed = currentFuel;
-                }
-                totalFuelOfFuel += moduleFuel;
-                
-            }
-        } catch (e:haxe.io.Eof) {
-            fileIn.close();
-        }
-        part2Answer = totalFuelOfFuel;
-        var stopStamp = Timer.stamp();
+		var totalFuel = 0;
+		var totalFuelOfFuel = 0;
+		try {
+			while (true) {
+				var line = fileIn.readLine();
+				var needed = Std.parseInt(line);
+				totalFuel += Math.floor(needed / 3) - 2;
+				totalFuelOfFuel += Main.part2Recursion(needed, 0);
+			}
+		} catch (e:haxe.io.Eof) {
+			fileIn.close();
+		}
 
-        trace("Part 1: " + part1Answer);
-        trace("Part 2: " + part2Answer);
-        trace("Time in seconds it took to run: " + (stopStamp-stamp));
-    }
+		part1Answer = totalFuel;
+		part2Answer = totalFuelOfFuel;
+
+		var stopStamp = Timer.stamp();
+
+		Sys.println("Part 1 Answer: " + part1Answer);
+		Sys.println("Part 2 Answer: " + part2Answer);
+		Sys.println("Time in seconds it took to run: " + (stopStamp - stamp));
+	}
 }
