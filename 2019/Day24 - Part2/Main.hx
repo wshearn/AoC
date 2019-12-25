d877bb014b7b32f6aa8182818717473947e5fd0a
Mighty Derp <themightyderp@dirtyninjagames.io>
2019-12-25 01:15:39 -0500

    Day24 Part2

---
+++ 2019/Day24 - Part2/Main.hx
@@ -0,0 +1,158 @@
+import haxe.Timer;
+import sys.io.File;
+
+class Main {
+	public static function main() {
+		var part2Answer = 0;
+
+		var stamp = Timer.stamp();
+		var raw_data = File.getContent("input").split("\n");
+
+		var data:Array<Map<Int, Array<Array<Int>>>> = new Array<Map<Int, Array<Array<Int>>>>();
+		data[0] = new Map<Int, Array<Array<Int>>>();
+		data[1] = new Map<Int, Array<Array<Int>>>();
+		data[0][0] = [for (row in 0...5) [for (col in 0...5) 0]];
+		data[1][0] = [for (row in 0...5) [for (col in 0...5) 0]];
+
+		for (row in 0...5) {
+			for (col in 0...5) {
+				if (raw_data[row].charCodeAt(col) == "#".code) {
+					data[0][0][row][col] = 1;
+					data[1][0][row][col] = 1;
+				} else {
+					data[0][0][row][col] = 0;
+					data[1][0][row][col] = 0;
+				}
+			}
+		}
+
+		var whichData = 0;
+		for (time in 0...200) {
+			part2Answer = 0;
+
+			var nextData = whichData - 1;
+			if (nextData < 0) {
+				nextData = 1;
+			}
+
+			for (tile in data[whichData].keys()) {
+				if (!data[whichData].exists(tile - 1)) {
+					var colCount:Int = 0;
+					for (x in 0...5) {
+						colCount += data[whichData][tile][0][x];
+						colCount += data[whichData][tile][4][x];
+						colCount += data[whichData][tile][x][0];
+						colCount += data[whichData][tile][x][4];
+					}
+
+					if (colCount != 0) {
+						data[0][tile - 1] = [for (x in 0...5) [for (y in 0...5) 0]];
+						data[1][tile - 1] = [for (x in 0...5) [for (y in 0...5) 0]];
+					}
+				}
+
+				if (!data[whichData].exists(tile + 1)) {
+					var innerCount:Int = 0;
+					innerCount += data[whichData][tile][2][1];
+					innerCount += data[whichData][tile][2][3];
+					innerCount += data[whichData][tile][1][2];
+					innerCount += data[whichData][tile][3][2];
+
+					if (innerCount != 0) {
+						data[0][tile + 1] = [for (x in 0...5) [for (y in 0...5) 0]];
+						data[1][tile + 1] = [for (x in 0...5) [for (y in 0...5) 0]];
+					}
+				}
+			}
+
+			for (tile in data[whichData].keys()) {
+				for (row in 0...5) {
+					for (col in 0...5) {
+						if (row == 2 && col == 2) { // Mid point is a teleporter
+							continue;
+						}
+
+						var totalBugs:Int = 0;
+
+						// Top
+						if (row == 0) {
+							if (data[whichData].exists(tile - 1)) {
+								totalBugs += data[whichData][tile - 1][1][2];
+							}
+						} else if (row == 3 && col == 2) {
+							if (data[whichData].exists(tile + 1)) {
+								for (x in 0...5) {
+									totalBugs += data[whichData][tile + 1][4][x];
+								}
+							}
+						} else {
+							totalBugs += data[whichData][tile][row - 1][col];
+						}
+
+						// Bottom
+						if (row == 4) {
+							if (data[whichData].exists(tile - 1)) {
+								totalBugs += data[whichData][tile - 1][3][2];
+							}
+						} else if (row == 1 && col == 2) {
+							if (data[whichData].exists(tile + 1)) {
+								for (x in 0...5) {
+									totalBugs += data[whichData][tile + 1][0][x];
+								}
+							}
+						} else {
+							totalBugs += data[whichData][tile][row + 1][col];
+						}
+
+						// Left
+						if (col == 0) {
+							if (data[whichData].exists(tile - 1)) {
+								totalBugs += data[whichData][tile - 1][2][1];
+							}
+						} else if (row == 2 && col == 3) {
+							if (data[whichData].exists(tile + 1)) {
+								for (x in 0...5) {
+									totalBugs += data[whichData][tile + 1][x][4];
+								}
+							}
+						} else {
+							totalBugs += data[whichData][tile][row][col - 1];
+						}
+
+						// Right
+						if (col == 4) {
+							if (data[whichData].exists(tile - 1)) {
+								totalBugs += data[whichData][tile - 1][2][3];
+							}
+						} else if (row == 2 && col == 1) {
+							if (data[whichData].exists(tile + 1)) {
+								for (x in 0...5) {
+									totalBugs += data[whichData][tile + 1][x][0];
+								}
+							}
+						} else {
+							totalBugs += data[whichData][tile][row][col + 1];
+						}
+
+						if (data[whichData][tile][row][col] != 0 && totalBugs != 1) {
+							data[nextData][tile][row][col] = 0;
+						} else if (data[whichData][tile][row][col] == 0 && (totalBugs == 1 || totalBugs == 2)) {
+							data[nextData][tile][row][col] = 1;
+						} else {
+							data[nextData][tile][row][col] = data[whichData][tile][row][col];
+						}
+
+						part2Answer += data[nextData][tile][row][col];
+					}
+				}
+			}
+
+			whichData = nextData;
+		}
+
+		var stopStamp = Timer.stamp();
+
+		trace("Part 2: " + part2Answer);
+		trace("Time in seconds it took to run: " + (stopStamp - stamp));
+	}
+}
