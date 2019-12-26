7376b7b00f44ec42ea70a5dd9925b058e03f63f3
Mighty Derp <themightyderp@dirtyninjagames.io>
2019-12-26 00:39:13 -0500

    Added Day22 Part 1

---
+++ 2019/Day22 - Part1/Main.hx
@@ -0,0 +1,88 @@
+import haxe.Timer;
+import sys.io.File;
+
+class Main {
+	public static function technique_cut(deck:Array<Int>, size:Int, ?rule:String):Array<Int> {
+		var pos:Int = 0;
+		var newDeck:Array<Int> = new Array<Int>();
+		newDeck[deck.length - 1] = -1;
+
+		if (size < 0) {
+			var start = deck.length + size;
+			for (x in start...deck.length) {
+				newDeck[pos] = deck[x];
+				pos++;
+			}
+			for (x in 0...start) {
+				newDeck[pos] = deck[x];
+				pos++;
+			}
+		} else {
+			for (x in size...deck.length) {
+				newDeck[pos] = deck[x];
+				pos++;
+			}
+			for (x in 0...size) {
+				newDeck[pos] = deck[x];
+				pos++;
+			}
+		}
+
+		return newDeck;
+	}
+
+	public static function technique_stack(deck:Array<Int>):Array<Int> {
+		deck.reverse();
+
+		return deck;
+	}
+
+	public static function technique_increment(deck:Array<Int>, size:Int):Array<Int> {
+		var pos:Int = 0;
+		var newDeck:Array<Int> = new Array<Int>();
+		newDeck[deck.length - 1] = -1;
+
+		for (x in 0...deck.length) {
+			newDeck[pos] = deck[x];
+			pos += size;
+			while (pos > deck.length) {
+				pos = pos - deck.length;
+			}
+		}
+
+		return newDeck;
+	}
+
+	public static function main() {
+		var stamp = Timer.stamp();
+
+		var part1Answer = 0;
+
+		var inputFile:String = "input";
+
+		var raw_data = File.getContent(inputFile);
+		var shuffle = raw_data.split("\n");
+
+		// Part 1
+		var numOfCards:Int = 10007;
+		var deck:Array<Int> = [for (i in 0...numOfCards) i];
+		for (rule in shuffle) {
+			var num = rule.lastIndexOf(" ") + 1;
+			if (rule == "deal into new stack") {
+				deck = Main.technique_stack(deck);
+			} else if (StringTools.startsWith(rule, "deal with increment")) {
+				deck = Main.technique_increment(deck, Std.parseInt(rule.substring(num)));
+			} else if (StringTools.startsWith(rule, "cut")) {
+				deck = Main.technique_cut(deck, Std.parseInt(rule.substring(num)));
+			} else {
+				trace("OOPS");
+			}
+		}
+		part1Answer = deck.lastIndexOf(2019);
+		// End Part 1
+
+		var stopStamp = Timer.stamp();
+		trace("Part 1: " + part1Answer);
+		trace("Time in seconds it took to run: " + (stopStamp - stamp));
+	}
+}
--- 2019/Day24 - Part2/Main.hx
+++ 2019/Day24 - Part2/Main.hx
@@ -44,59 +44,55 @@ class Main {
 
 			var nextData = (whichData + 1) % 2;
 
-			if (!data[whichData].exists(lowestKey - 1)) {
-				var colCount:Int = 0;
-				for (x in 0...5) {
-					colCount += data[whichData][lowestKey][0][x];
-					colCount += data[whichData][lowestKey][4][x];
-					colCount += data[whichData][lowestKey][x][0];
-					colCount += data[whichData][lowestKey][x][4];
-				}
-
-				if (colCount != 0) {
-					data[0][lowestKey - 1] = [
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0]
-					];
-					data[1][lowestKey - 1] = [
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0]
-					];
-
-					lowestKey = lowestKey - 1;
-				}
+			var outterCount:Int = 0;
+			for (x in 0...5) {
+				outterCount += data[whichData][lowestKey][0][x];
+				outterCount += data[whichData][lowestKey][4][x];
+				outterCount += data[whichData][lowestKey][x][0];
+				outterCount += data[whichData][lowestKey][x][4];
 			}
 
-			if (!data[whichData].exists(highestKey + 1)) {
-				var innerCount:Int = 0;
-				innerCount += data[whichData][highestKey][2][1];
-				innerCount += data[whichData][highestKey][2][3];
-				innerCount += data[whichData][highestKey][1][2];
-				innerCount += data[whichData][highestKey][3][2];
+			if (outterCount != 0) {
+				data[0][lowestKey - 1] = [
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0]
+				];
+				data[1][lowestKey - 1] = [
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0]
+				];
 
-				if (innerCount != 0) {
-					data[0][highestKey + 1] = [
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0]
-					];
-					data[1][highestKey + 1] = [
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0],
-						[0, 0, 0, 0, 0]
-					];
-					highestKey = highestKey + 1;
-				}
+				lowestKey = lowestKey - 1;
+			}
+
+			var innerCount:Int = 0;
+			innerCount += data[whichData][highestKey][2][1];
+			innerCount += data[whichData][highestKey][2][3];
+			innerCount += data[whichData][highestKey][1][2];
+			innerCount += data[whichData][highestKey][3][2];
+
+			if (innerCount != 0) {
+				data[0][highestKey + 1] = [
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0]
+				];
+				data[1][highestKey + 1] = [
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0]
+				];
+				highestKey = highestKey + 1;
 			}
 
 			for (tile in data[whichData].keys()) {
